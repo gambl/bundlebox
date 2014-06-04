b82b63e424ddb2bf627160adcb979a0c6cbe4910
Matthew Gamble <matthewgamble@lem.local>
2014-06-04 15:45:29 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 bundlebox
 =========
 
+Flask based server to manage Dropbox Webhooks for Research Object management. 
  
---
+++ bundlebox.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/verify", methods=["GET"])
+def verify():
+    '''Verify that the application is expecting notifactions from dropbox webhooks by returing the challenge'''
+
+    return request.args.get('challenge')
+
+@app.route("/hello")
+def hello():
+    '''Verify that the application is expecting notifactions from dropbox webhooks by returing the challenge'''
+
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=False)
